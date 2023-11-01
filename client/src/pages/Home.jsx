08861c3b39ab6fb82440bbe9f187a9670254d46b
PiotrDrechsler <pdrech@gmail.com>
2023-11-01 17:37:40 +0100

    fix/Home.js and Card.jsx and utils
    Share generated image with community.
    
    Home.jsx
    fetchPosts - get all posts from mongoDBm and render them reverse by id and their url
    
    utils
    import FileSaver
    async founction to download image
    
    Card.jsx
    import download icon
    import downloadImaga from utils
    create Card styles in talwind
    use props _id, name, prompt and photo
    create  card info with name, prompt and downliad img showing on hover
    add download photo link to icon

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,11 +11,36 @@ const RenderCards = ({ data, title }) => {
     <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">{title}</h2>
   );
 };
-
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState("");
+
+  const fetchPosts = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch("http://localhost:8080/api/v1/post", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse());
+      }
+    } catch (err) {
+      alert(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -46,9 +71,12 @@ const Home = () => {
             )}
             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
               {searchText ? (
-                <RenderCards data={[]} title="No Search Results Found" />
+                <RenderCards
+                  data={[setSearchText]}
+                  title="No Search Results Found"
+                />
               ) : (
-                <RenderCards data={[]} title="No Posts Found" />
+                <RenderCards data={allPosts} title="No Posts Found" />
               )}
             </div>
           </>
