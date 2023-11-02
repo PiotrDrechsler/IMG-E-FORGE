42684016e8f1eeac13e36d9ea71ff15e4ce6b1a4
PiotrDrechsler <pdrech@gmail.com>
2023-11-02 16:53:13 +0100

    Update index.js
    Cors-bug

--- server/index.js
+++ server/index.js
@@ -9,9 +9,14 @@ import replicateRoutes from "./routes/replicateRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(express.json({ limit: "50mb" }));
 
+const corsConfig = {
+  origin: "https://https://img-e-forge.netlify.app/",
+  credentials: true,
+};
+app.use(cors(corsConfig));
+app.options("*", cors(corsConfig));
+app.use(express.json({ limit: "50mb" }));
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/replicate", replicateRoutes);
 
