e1051bf2d7ed41e56076e7b5aa060b9bae93a787
PiotrDrechsler <pdrech@gmail.com>
2023-11-02 16:36:52 +0100

    Styles
    Improve the readability and layout of the website has been improved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,15 +7,15 @@ import { Home, CreatePost } from "./pages";
 const App = () => {
   return (
     <BrowserRouter>
-      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
-        <Link to="/">
-          <img src={logo} alt="logo" className="w-4/12 object-contain" />
+      <header className="w-full flex flex-col items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+        <Link to="/" className="w-60 my-8 object-contain ">
+          <img src={logo} alt="logo" />
         </Link>
         <Link
           to="/create-post"
-          className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+          className="font-inter font-medium bg-black text-white px-20 py-6 rounded-md hover:bg-neutral-700"
         >
-          Create
+          Start creating!
         </Link>
       </header>
       <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -93,7 +93,7 @@ const CreatePost = () => {
           the community
         </p>
       </div>
-      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+      <form className="mt-8 max-w-3xl" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-5">
           <FormField
             labelName="Your Name"
@@ -113,6 +113,15 @@ const CreatePost = () => {
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
+          <div className="flex gap-5">
+            <button
+              type="button"
+              onClick={generateImage}
+              className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            >
+              {generatingImg ? "Generating..." : "Generate"}
+            </button>
+          </div>
           <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
             {form.photo ? (
               <img
@@ -135,17 +144,8 @@ const CreatePost = () => {
             )}
           </div>
         </div>
-        <div className="mt-5 flex gap-5">
-          <button
-            type="button"
-            onClick={generateImage}
-            className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-          >
-            {generatingImg ? "Generating..." : "Generate"}
-          </button>
-        </div>
 
-        <div className="mt-10">
+        <div className="mt-5">
           <p className="mt-2 text-[#666e75] text-[14px]">
             ** Once you have created the image you want, you can share it with
             others in the community **
